125153507e644f45d9bd64498cbb592d215be5b2
Liliana <molilis.pr95@live.com.mx>
2021-04-22 19:00:07 -0500

    fix

--- src/app/store/store-config.module.ts
+++ src/app/store/store-config.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../../environments/environment';
 import { ROOT_EFFECTS } from './effects';
 import { ROOT_REDUCERS } from './reducers';
 
@@ -20,7 +19,6 @@ import { ROOT_REDUCERS } from './reducers';
     }),
     StoreDevtoolsModule.instrument({
       name: 'Pokemon',
-      logOnly: environment.production,
     }),
     EffectsModule.forRoot(ROOT_EFFECTS),
   ],
